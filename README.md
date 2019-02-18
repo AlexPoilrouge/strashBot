2366a55d157b1a2b2086da1385f4746aa06d64e9
Poilrouge <alexandre.hurstel@mailoo.org>
2019-02-18 13:16:36 +0100

    the first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+#### INFO
+
+Bot made for small local Discord server.
+Can (non-exhaustively):
+* Add specific role to a user that reacts with a specific emoji to a specific message
+* Respond to some command under format `!command`
+(only works for one server per instance)
+Bot written in node.js.
+
+
+
+#### Dependencies
+
+* npm
+* Node.js
+* discord.js
+* node-config
+
+
+
+#### Install and use
+
+##### Create and invite Bot
+
+Check out Discord's documentation for how to [create a bot account](https://discordpy.readthedocs.io/en/rewrite/discord.html) for your server.
+Once it's done, invite the bot to the server (only possible by user with "manager server" permission) by using a link formatted as follow:
+`https://discordapp.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&scope=bot&permissions=PERMISSION_INTEGER`
+Where `YOUR_CLIENT_ID` can be found on the [discord application page](https://discordapp.com/developers/applications), and the `PERMISSION_INTEGER` can also be compute from there ('Bot' section).
+
+**Be sure to note your bot's `TOKEN` from the bot section.**
+
+
+##### Configure bot
+
+Configure the bot by filling appropriately the `config/default.json` file.
+The `token` field is your bot's previously obtained token.
+All the IDs can be found from your Discord App, by switching to developer mode.
+
+Alternatively, you can configure the bot using the `NODE_CONFIG` env variable:
+
+`export NODE_CONFIG='{"StrashBot":{"version":"0.1.0","build" … }}'`
+
+
+##### Launch bot
+
+`node bot.js`
+
+
+
+#### Release and use
+
+This bot was made for personal needs and use. The code is release on the
+off chance it might be of use to someone but without the intention of providing
+any form of utility software or service in a rigorous manner.
+Therefore, **no support** is endorsed by the developer, meaning that **any
+comment, feedback, or request regarding this code should be expected to be completely
+ignored by the developer**.
+Additionally, the responsibility of any undesired effect the execution of this
+bot might have on any system lies solely in the hands of the user.
+
+
+*From Strasbourg,
+with love.*
+
---
+++ bot.js
@@ -0,0 +1,108 @@
+// Import the discord.js module
+const Discord= require('discord.js');
+
+const wk= require('./worker');
+const utils= require('./utils')
+
+const config= require('config');
+
+class SmashBot extends Discord.Client{
+    constructor(){
+        super();
+
+        this.worker= new wk.Worker(this);
+
+        this.worker.guildID= config.get('StrashBot.smashGuildID');
+        this.worker.botChannel= config.get('StrashBot.botChannelID');
+        this.worker.onlineChannel= config.get('StrashBot.online.channelID');
+        this.worker.onlineReacMessage= config.get('StrashBot.online.reacMessageID');
+        this.worker.onlineReacEmote= config.get('StrashBot.online.availableEmoteID');
+        this.worker.onlineRole= config.get('StrashBot.online.roleID');
+
+        this.valid= utils.JSONCheck.validity(config.get('StrashBot'));
+    }
+
+    login(token){
+        if(!this.valid){
+            console.log( utils.JSONCheck.report(config.get('StrashBot')) );
+            console.log("bot config isn't valid, won't login to discord");
+        }
+        else{
+            super.login(token)
+            .then()
+            .catch( err => { console.log("Error when login to discord attempt…"); console.log(err); });
+        }
+    }
+
+    get version(){
+        return config.get('StrashBot.version')+'-'+config.get('StrashBot.build');
+    }
+};
+
+
+const client= new SmashBot();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', ()=>{
+    console.log("Pif paf! StrashBot rrrready to rumblllllllllle!");
+    
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name)
+    })
+
+    client.worker.ready_setup();
+});
+
+client.on('message', (message)=>{
+    if(message.author.id === client.user.id) return; // Prevent bot from responding to its own messages
+
+
+    if(message.channel.type === 'dm'){
+        console.log(`Recieving DM command from ${message.author.id}`);
+        client.worker.dMessage(message);
+    }
+    else{
+        console.log(`Recieving command from channel ${message.channel.id}`);
+        client.worker.processMessage(message);
+    }
+});
+
+client.on('messageReactionAdd', (reaction, user) => {
+	client.worker.reactionAdd(reaction, user)
+});
+
+client.on('messageReactionRemove', (reaction, user) => {
+	client.worker.reactionRemove(reaction, user);
+});
+
+client.on('error', (error)=>{
+    console.log("SmashBot websocket encountered an error…");
+    console.log(error);
+});
+
+client.on('reconnecting', ()=>{
+    console.log("SmashBot is attempting a reconnection through websocket…");
+    client.worker.destroy();
+});
+
+client.on('resume', (replayed) =>{
+    client.worker.destroy();
+    console.log("SmashBot's websocket is resuming… "+replayed+" events were played.");
+});
+
+client.on('warn', (info) =>{
+    console.log("SmashBot WARNING!!! : "+info);
+});
+
+client.on('disconnect', (event)=>{
+    client.worker.destroy();
+    console.log("SmashBot disconnected.");
+    console.log(event);
+});
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login(config.get('StrashBot.token'));
